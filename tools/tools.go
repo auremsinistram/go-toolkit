6d1e9298c166d8fc147ead7f0af3d966c820d217
auremsinistram <auremsinistram@gmail.com>
2026-08-26 23:06:02 +0300

    feat: add generate code function

--- tools/tools.go
+++ tools/tools.go
@@ -1,8 +1,13 @@
 package tools
 
 import (
+	"crypto/rand"
+	"math/big"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/auremsinistram/go-errors"
 )
 
 func GetenvInt(key string, defaultValue int) int {
@@ -48,4 +53,27 @@ func GetenvBool(key string, defaultValue bool) bool {
 	}
 
 	return result
+}
+
+func GenerateCode(charset string, length int) (string, error) {
+	if length <= 0 || length > 100 {
+		return "", errors.New("length is invalid")
+	}
+
+	var result strings.Builder
+
+	result.Grow(length)
+
+	max := big.NewInt(int64(len(charset)))
+
+	for range length {
+		random, err := rand.Int(rand.Reader, max)
+		if err != nil {
+			return "", errors.Wrap(err, "tools - GenerateCode - #1")
+		}
+
+		result.WriteByte(charset[random.Int64()])
+	}
+
+	return result.String(), nil
 }
